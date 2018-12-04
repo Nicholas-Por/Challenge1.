91916963c79853ad677d82036a16b48760142b54
Nicholas-Por <nporzio3890@bths.edu>
2018-12-04 15:23:48 -0500

    Challenge 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,70 +2,82 @@ package com.company;
 import java.util.ArrayList;
 public class Main
 {
-    public static void main(String[] args)
-    {
-        int[] temp1 = {};
-        Arr(10000);
-        int[] array = new int[Arry.length];
-        IntMerge(array, 0, 0, 0, temp1);
-    }
-    public static int[] Arr(int num)
-    {
-        int Arry[] = {};
-        for(int i = 0; i <= num;i++)
-        {
-            Arry[i] = (int)(Math.random()*10);
-        }
-        return Arry;
-    }
-   int i; //left
-   int j; //middle
-   int k; //also left to track number placement
-    int right;
-    public static void IntMerge(int[] Arr, int left, int mid,int right, int[] temp){
-        int i = left;
-        int j = mid + 1;
-        int k = left;
 
-        while( i <= mid && j <= right){
-            if(Arr[i] <=(Arr[j]))
-            {
-                temp[k] = Arr[i];
-                i++;
-            }else{
-                temp[k] = Arr[j];
-                j++;
+    static double findMedian(int[] values) {
+        double median;
+        // get count of scores
+        int totalElements = values.length;
+        // check if total number of scores is even
+        if (totalElements % 2 == 0) {
+            int sumOfMiddleElements = values[totalElements / 2] + values[totalElements / 2 - 1];
+            // calculate average of middle elements
+            median = ((double) sumOfMiddleElements) / 2;
+        } else {
+            // get the middle element
+            median = (double) values[values.length / 2];
+        }
+        return median;
+    }
+    public static int[] fillArray(int length){
+        int chosenN = (int)(Math.random() * length + 1);
+        int[] array = new int[chosenN];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * length + 1);
+        }
+
+        return array;
+    }
+    public static int[] mergeSort(int[] arr) {
+        int number = arr.length;
+        if (number == 1) return arr;
+        int secondNumber = (int) (number / 2.0 + 0.5);
+
+        int[] arrayHalfOne = new int[secondNumber];
+        int[] arrayHalfTwo = new int[number - secondNumber];
+
+        for (int i = 0; i < secondNumber; i++) {
+            arrayHalfOne[i] = arr[i];
+        }
+
+        for (int i = secondNumber; i < number; i++) {
+            arrayHalfTwo[i - secondNumber] = arr[i];
+        }
+
+        arrayHalfOne = mergeSort(arrayHalfOne);
+        arrayHalfTwo = mergeSort(arrayHalfTwo);
+
+        return merge(arrayHalfOne, arrayHalfTwo);
+    }
+    public static int[] merge(int[] arrayHalfOne, int[] arrayHalfTwo) {
+        int[] c = new int[arrayHalfOne.length + arrayHalfTwo.length];
+        int i0 = 0;
+        int i1 = 0;
+        int i2 = 0;
+
+        while (i1 < arrayHalfOne.length && i2 < arrayHalfTwo.length) {
+            if (arrayHalfOne[i1] > arrayHalfTwo[i2]) {
+                c[i0] = arrayHalfTwo[i2];
+                i2++;
+            } else {
+                c[i0] = arrayHalfOne[i1];
+                i1++;
             }
-            k++;
-        }
-        while(i <= mid){
-            temp[k] = Arr[i];
-            i++;
-            k++;
-        }
-        while(j <= right){
-            temp[k] = Arr[j];
-            j++;
-            k++;
-        }
-        for(k = left; k <= right; k++){
-            Arr[k] = temp[k];
+            i0++;
         }
 
-    }
-    public static void IntMergeSortHelper(int[] elements, int from, int to, int[] temp){
-        if(from < to){
-            int middle = (from + to) / 2;
-            IntMergeSortHelper(elements, from, middle, temp);
-            IntMergeSortHelper(elements,middle+1,to,temp);
-            IntMerge(elements, from, middle, to, temp);
+        while (i1 < arrayHalfOne.length) {
+            c[i0] = arrayHalfOne[i1];
+            i0++;
+            i1++;
         }
-    }
 
-    public static void IntMergeSort(int[] elements){
-        int n = elements.length;
-        int[] temp = new int[n];
-        IntMergeSortHelper(elements,0,n-1,temp);
+        while (i2 < arrayHalfTwo.length) {
+            c[i0] = arrayHalfTwo[i2];
+            i0++;
+            i2++;
+        }
+        return c;
     }
 
 
